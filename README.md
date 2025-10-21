b484976369f520d4bb3a116fd931ee8efd73e6d0
irfanulHaqueMT <mtirfanulhaque@gmail.com>
2025-10-21 14:33:40 +0530

    first commint after signIn / signUp form validation

---
+++ README.md
@@ -0,0 +1,27 @@
+# Netflix GPT
+
+- create @latestVite app
+- configure TailwindCSS
+- Header
+- Routing of App
+- Login form
+- Sign up form
+- Form Validation
+- useRef Hook
+- Firebase Setup
+- Deploying our app to production
+- Create SignUp user account
+
+# features
+
+- Login / singnup
+  - Sign In / Sign Up form
+  - Redirect tp Browse Page
+- Browser (after authentication)
+
+  - Header
+    -Main Movie - Tailer in Background - Title & description - Movie suggestions - Movie list \* N
+
+- NetflixGPT
+  - Search Bar
+  - Movie Suggestions
---
+++ src/Components/Login.jsx
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const username = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    // validate the form data
+    console.log(username);
+    const message = checkValidData(
+      email.current.value,
+      password.current.value,
+      username.current.value
+    );
+    setErrorMessage(message);
+  };
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/d482944d-eab4-4a64-89c9-a07a508a6e42/web/IN-en-20250929-TRIFECTA-perspective_4cf0c8a1-bd35-4d72-a49f-165021531dde_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 p-12 my-36 absolute bg-black/75 right-0 left-0 mx-auto text-amber-50 rounded-lg"
+      >
+        <h1 className="text-3xl font-bold py-2 mb-2 ">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            ref={username}
+            type="text"
+            placeholder="Full name"
+            className="p-2 my-2 bg-gray-900 w-full"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email or mobile number"
+          className="p-2 my-2 bg-gray-900 w-full"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-2 my-2 bg-gray-900 w-full"
+        />
+        <p className="text-red-500 py-2 text-md">{errorMessage}</p>
+        <button
+          className="bg-red-700 text-white w-full p-2 my-2 rounded-md cursor-pointer"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-6 text-sm cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to NetflixGPT? Sign Up Now"
+            : "Already Registered? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
